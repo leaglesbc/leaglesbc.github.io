525996a08572eb080d3a10c2721dc0ba5d831cab
limeXDDDD <canoflime@gmail.com>
2017-10-20 16:43:13 +1300

    misc changes

--- pull.js
+++ pull.js
@@ -1,14 +1,14 @@
-var isOut = 0;
+var isOut = 1;
 var width = "140px";
 $( document ).ready(function() {
 $(".pull-out").css("top", "-" + width)
 	$(".pull-out").css("top", width)
 	$("#pull-button").click(function(){
 		if (isOut == 0) {
-			$(".pull-out").css("top","140px")
+			$(".pull-out").css("top", width)
 			isOut = 1;
 		}else if (isOut == 1){
-			$(".pull-out").css("top", "0")
+			$(".pull-out").css("top", "80px")
 			isOut = 0;
 		}
 	});
